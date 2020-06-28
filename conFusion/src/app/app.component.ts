e4437928d40eff4c1bc76b6115a66a9db3946c63
Cristian D. Ceballos <cdceballor@eafit.edu.co>
2020-06-27 21:04:33 -0500

    commit 2

--- conFusion/src/app/app.component.ts
+++ conFusion/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'conFusion';
+  title = 'My first page';
+  name = 'Cristian';
 }
